60cc1fb850a0e552abd2ad5778cc99b17ed328ba
cjeffreyRC <chris.jeffrey@robertsoncollege.net>
2016-03-07 09:48:01 -0600

    Revert "Revert "Moved to aspx page and added funtional database""
    This reverts commit 2910fdb4ac110f1ef53399f68afae122e2e4ff73.

--- Test/Test/ChatHub.cs
+++ Test/Test/ChatHub.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Data;
 
 namespace Test
 {
@@ -11,7 +12,18 @@ namespace Test
         public void Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
+            DAL d = new DAL();
+            d.AddParam("username", name);
+            d.AddParam("message", message);
+            d.ExecuteNonQuery("spSaveMessage");
             Clients.All.broadcastMessage(name, message);
+        }
+
+        public void SaveUser(string username)
+        {
+            DAL d = new DAL();
+            d.AddParam("username", username);
+            d.ExecuteNonQuery("spSaveUser");
         }
     }
 }
