9876e9fcc239aefb8a7b2fbdcded5c3d56ac729a
CjeffreyRC <chris.jeffrey@robertsoncollege.net>
2016-03-06 23:54:12 -0600

    Moved to aspx page and added funtional database

---
+++ Test/Test/default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Test
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DAL d = new DAL();
+            DataSet ds = d.ExecuteProcedure("spGetMessages");
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                string name = row["username"].ToString();
+                string message = row["message"].ToString();
+                discussion.InnerHtml += "<li><strong>" + name +  "</strong>:&nbsp;&nbsp;" + message + "</li>";
+            }
+        }
+    }
+}
